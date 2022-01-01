73bd97105d6fd58c067d4ee747bb1ed448acb7c5
Elton Santos <eltonaxl@hotmail.com>
2022-01-01 18:08:22 -0300

    Criando a lista de hero

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+  { id: 1, name: "Wolverine"},
+  { id: 2, name: "Thor"},
+  { id: 3, name: "Capitão América"},
+  { id: 4, name: "Magneto"},
+  { id: 5, name: "Batman"},
+  { id: 6, name: "Homem aranha"},
+  { id: 7, name: "Homem de ferro"},
+  { id: 8, name: "Hulk"},
+  { id: 9, name: "Super homem"},
+  { id: 10, name: "Mulher maravilha"},
+]
