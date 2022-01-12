a0a6dff566913c674b4b06e6e83c33305b7e9c76
Elton Santos <eltonaxl@hotmail.com>
2022-01-11 23:20:10 -0300

    Updating project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,8 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { MessagesComponent } from './messages/messages.component';
 import { MaterialModule } from './material/material.module';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
