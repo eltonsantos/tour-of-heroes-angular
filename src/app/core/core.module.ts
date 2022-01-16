934b1d38d936aaf0e07ece2aaf27ded1a152988e
Elton Santos <eltonaxl@hotmail.com>
2022-01-16 00:35:25 -0300

    Feature module and organization of the project

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,9 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from '../material/material.module';
 import { MessagesComponent } from './components/messages/messages.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { MaterialModule } from './material/material.module';
 
 const COMPONENTS = [MessagesComponent, ToolbarComponent];
 const MODULES = [FlexLayoutModule, MaterialModule, RouterModule];
@@ -18,7 +18,7 @@ export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule?: CoreModule) {
     if (parentModule) {
       throw new Error(
-        'CoreModule has already been loaded. Import this module in the AppModule.'
+        'CoreModule has already been loaded. Import this module in the AppModule only.'
       );
     }
   }
